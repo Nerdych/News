f7d03f4c2ee3f2b4359f5d1d21a71d9aef2a786a
Nikita Prodan <sa_vvk@mail.ru>
2021-09-17 21:10:20 +0300

    fix build

--- components/Atag/Atag.tsx
+++ components/Atag/Atag.tsx
@@ -3,18 +3,18 @@ import styles from './Atag.module.scss';
 import { AtagProps } from './Atag.props';
 import cn from 'classnames';
 
-export const Atag = React.forwardRef(
-	({ children, active = false, className, ...props }: AtagProps, ref): JSX.Element => {
-		return (
-			<a
-				className={cn(styles.a, className, {
-					[styles.active]: active,
-				})}
-				ref={ref}
-				{...props}
-			>
-				{children}
-			</a>
-		);
-	}
-);
+const Atag = React.forwardRef(({ children, active = false, className, ...props }: AtagProps, ref): JSX.Element => {
+	return (
+		<a
+			className={cn(styles.a, className, {
+				[styles.active]: active,
+			})}
+			ref={ref}
+			{...props}
+		>
+			{children}
+		</a>
+	);
+});
+
+export default Atag;
