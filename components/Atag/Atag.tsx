9eabcf352cd1fee05f345dd335e81c90ec26b54c
Nikita Prodan <sa_vvk@mail.ru>
2021-09-17 21:23:30 +0300

    fix build

--- components/Atag/Atag.tsx
+++ components/Atag/Atag.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 import styles from './Atag.module.scss';
 import { AtagProps } from './Atag.props';
 import cn from 'classnames';
 
-const Atag = React.forwardRef(({ children, active = false, className, ...props }: AtagProps, ref): JSX.Element => {
-	return (
-		<a
-			className={cn(styles.a, className, {
-				[styles.active]: active,
-			})}
-			ref={ref}
-			{...props}
-		>
-			{children}
-		</a>
-	);
-});
+const Atag = React.forwardRef(
+	(
+		{ children, active = false, className, ...props }: AtagProps,
+		ref: ForwardedRef<HTMLAnchorElement>
+	): JSX.Element => {
+		return (
+			<a
+				className={cn(styles.a, className, {
+					[styles.active]: active,
+				})}
+				ref={ref}
+				{...props}
+			>
+				{children}
+			</a>
+		);
+	}
+);
 
+Atag.displayName = 'Atag';
 export default Atag;
